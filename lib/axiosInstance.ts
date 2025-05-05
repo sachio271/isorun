a7cc83ca72dda3041975fe91273a51338281c5df
sachio271 <105487625+sachio271@users.noreply.github.com>
2025-05-05 11:09:21 +0700

    config

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:8000/api",
   
-  baseURL: "http://isoplusrun.wingssurya.com:8000/api",
+  baseURL: "http://isoplusrun.wingssurya.com/api",
   headers: {
     "Content-Type": "application/json",
   },
