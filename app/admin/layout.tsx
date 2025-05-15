34f95aaa7fde71ae95ab60006eca5644a895a7fe
sachio271 <105487625+sachio271@users.noreply.github.com>
2025-05-15 16:46:26 +0700

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,14 @@
 import Header from '@/components/header'
-import { ReactNode } from 'react'
+import { ReactNode, Suspense } from 'react'
 
 const AuthLayout = ({children} : {children : ReactNode}) => {
   return (
     <div className="min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/banner.webp')" }}>
         <Header />
         <div className="backdrop-blur-sm min-h-screen bg-white/30 p-10 sm: py-15 px-5 md:p-18">
+        <Suspense fallback={<div>Loading...</div>}>
             {children}
+        </Suspense>
         </div>
     </div>
   )
