fa188aa633311baaf3e0505dd1cc65cda3a5de51
sachio271 <105487625+sachio271@users.noreply.github.com>
2026-04-24 08:48:16 +0700

    fix next auth

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./lib/zod";
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:3000/api";
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   pages: {
     signIn: "/login",
   },
